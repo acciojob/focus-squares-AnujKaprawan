bd77b564261cb788263ed952c13097b1760d5055
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-03-02 20:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(quare => {
+  quare.addEventListener("mouseover", () => {
+    squares.forEach(otherSquare => {
+      if (otherSquare !== quare) {
+        otherSquare.style.backgroundColor = "#6F4E37"; /* Coffee */
+      }
+    });
+  });
+
+  quare.addEventListener("mouseout", () => {
+    squares.forEach(otherSquare => {
+      otherSquare.style.backgroundColor = "#E6E6FA"; /* Lavender */
+    });
+  });
+});
